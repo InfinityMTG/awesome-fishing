37bb2fbba8dd2ac85aba520c3fcdcf3b3393c8ee
yourunclesdog <konpeito.cyou>
2024-01-27 11:17:04 +0000

    Actually added sig_fig bwl

--- src/main.lua
+++ src/main.lua
@@ -5,11 +5,16 @@ function love.load()
 end
 
 function love.draw()
-  love.graphics.draw(rod.texture, 100*rod.x, rod.y)
+  love.graphics.draw(rod.texture, 200+100*math.sin(rod.x), rod.y)
 end
 
 function love.update()
-  rod.x = sig_fig(math.sin((rod.x + 0.1) % (math.pi * 2)), 2)
+  rod.x = sig_fig((rod.x + 0.1) % (math.pi * 2))
   print(rod.x, rod.y)
+end
+
+function sig_fig(n)
+  return tonumber(string.format("%.3f", n))
+  -- change the 3 in 3f to set the amount of sig figs.
 end
 
